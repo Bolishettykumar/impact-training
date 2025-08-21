7827f64ce94d3ba512f829d585dbd20a73f1a883
srinivas kumar <srinivaskumarbolishetty0209@gmail.com>
2025-08-21 10:11:22 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class loop{
+    public static void main(Strings[] args){
+        Scanner sc = new Scanner(System.in);
+        // for(int i = 0; i < 11; i++){
+        //     //counter++ => counter = counter + 1
+        //     //counter-- => counter = counter - 1 
+        //     // System.out.println(counter);
+        //     System.out.print(i+" ");
+        // }
+        // int i = 12;
+        // while(i < 11){
+        //     System.out.println(i);
+        //     i++;
+        // }
+        // do { 
+        //     System.out.println("Srinivas");
+        // } while (i < 11);
+        int n = sc.nextInt();
+
+        int sum = 0;
+
+        for(int i = 1; i<11; i++){
+            System.out.println(i*n);
+        }
+        
+        
+
+    }
+
+
+
+}
