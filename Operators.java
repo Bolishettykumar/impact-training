ac697f903c4ee7be7701363215c7713b0ee12819
srinivas kumar <srinivaskumarbolishetty0209@gmail.com>
2025-08-26 11:02:50 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Operators {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = 10; 
+        int b = 5;
+        //unary operators
+        // b = a++;
+        // b = ++a;
+        // b = --a;
+        // b = a--;
+
+        //Reational operators
+
+        // if(a>b){
+        if(a<b){
+            System.out.println(true);
+        }else{
+            System.out.println(false);
+        }
+
+       
+    }
+    
+}
